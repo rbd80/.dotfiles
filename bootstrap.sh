1dc8cf0d02669825f5ddea71eba58fc5940fd5e7
austinsilver <robert.donovan@rxd.io>
2018-06-09 09:46:10 -0400

    Massive update to how everything is laid out and updated how tools are updated too

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `osxprep.sh` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 function display {
 	echo -e "\n$1\n"
@@ -90,7 +95,7 @@ case $1 in
 		runAll
 		;;
 	* )
-		echo -e "usage: up all	- run all commands"
-		echo -e "usage: up stow	- just run the stow command"
+		echo -e "usage: bootstrap all	- run all commands"
+		echo -e "usage: bootstrap stow	- just run the stow command"
 		;;
 esac
